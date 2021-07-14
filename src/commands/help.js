a49bfc98235fde7f2351fd450b4b8e6c7a28ff20
Scorz <40724848+GitScorz@users.noreply.github.com>
2021-07-14 22:31:01 +0100

    Update help.js

--- src/commands/help.js
+++ src/commands/help.js
@@ -14,7 +14,7 @@ module.exports = {
 		if (args.length === 0) {
 			let e = new MessageEmbed()
 				.setAuthor("Help", user.displayAvatarURL())
-				.setDescription("Guild prefix is: " + prefix + "\nUse `" + prefix + "help <command>` to receive help for any command!")
+				.setDescription("The current guild prefix is: `" + prefix + "`\nUse `" + prefix + "help <command>` to receive help for any command!")
 				.setColor([11, 61, 94])
 				.addField("General", "`help`, `ping`, `report`")
 				.addField("Game", "`create`, `alliance`, `daily`, `match`, `inventory`, `profile`, `train`, `crates`")
@@ -29,7 +29,7 @@ module.exports = {
 				if (!cmd) return ErrorEmbed(message.channel, user, "no such command `" + command + "`.")
 
 				let h = new MessageEmbed()
-					.setAuthor("Help for " + cmd)
+					.setAuthor("Help for " + command)
 					.setColor([11, 61, 94])
 					.setDescription(`\nDescription: \`${cmd.description}\``
 					+ `\nUsage: \`${cmd.usage}\``);
